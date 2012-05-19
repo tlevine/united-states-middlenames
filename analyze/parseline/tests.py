6e40dfb2370cd433ff2ba35817b055d85be1f248
Thomas Levine <thomas.levine@gmail.com>
2012-05-19 18:56:50 -0400

    another test

--- analyze/parseline/tests.py
+++ analyze/parseline/tests.py
@@ -4,7 +4,14 @@ import datetime
 
 class BaseLineTest(unittest.TestCase):
     line = ''
-    expected_result = {}
+    expected_result = {
+        'ssn': '',
+        'surname': '',
+        'forename': '',
+        'middles': []
+        'born': {'year': None, 'month': None, 'day': None},
+        'died': {'year': None, 'month': None, 'day': None},
+    }
 
     def test_valid_line(self):
         '''
@@ -19,6 +26,7 @@ class BaseLineTest(unittest.TestCase):
         self.assertDictEqual(parseline(line), expected_result)
 
 class Test001010001(BaseLineTest):
+    'This is the first one! It has a missing value.'
     line = ' 001010001MUZZEY                  GRACE                          1200197504161902                   '
     expected_result = {
         'ssn': '001010001',
@@ -27,6 +35,17 @@ class Test001010001(BaseLineTest):
         'middles': []
         'born': {'year': 1902, 'month': 4, 'day': 16},
         'died': {'year': 1975, 'month': 12, 'day': None}, #Missing value
+    }
+
+class Test242250680(BaseLineTest):
+    line = ' 242250680TATE                    RONNIE         D              V1113200211161957                   '
+    expected_result = {
+        'ssn': '242250680',
+        'surname': 'TATE',
+        'forename': 'RONNIE',
+        'middles': ['D', 'V']
+        'born': {'year': 1957, 'month': 11, 'day': 16},
+        'died': {'year': 2002, 'month': 11, 'day': 13}, #Missing value
     }
 
 if __name__ == 'main':

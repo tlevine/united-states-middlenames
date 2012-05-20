0d005c8bfe80ef2a157e534ea506ebdda43bdc0d
Thomas Levine <thomas.levine@gmail.com>
2012-05-20 16:46:16 -0400

    separate worker bits

---
+++ worker/dates.py
@@ -0,0 +1,32 @@
+import datetime
+
+def process_date(datedict, datetype)
+    '''
+    Convert a date dictionary into date, day of week
+    and day of year, handling missing values.
+    '''
+
+    keys = set(datedict.keys())
+    if keys == {'year', 'month', 'day'}:
+        out = {
+            datetype + '.date'] = datetime.date(date['year'], date['month'], date['day'])
+            datetype + '.dow'] = out[datetype + '.date'].strftime('%a')
+        #   datetype + '.doy'] = None
+        }
+    elif keys == {'month', 'year'}:
+        out = {
+            datetype + '.date'] = datetime.date(date['year'], date['month'], 15)
+        #   datetype + '.dow'] = None
+            datetype + '.doy'] = out[datetype + '.date'].strftime('%j')
+        }
+    elif keys == {'month', 'day'}:
+        out = {
+            datetype + '.date': datetime.date(2000, date['month'], date['day']),
+        #   datetype + '.dow': None,
+            datetype + '.doy': out[datetype + '.date'].strftime('%j'),
+        }
+    else:
+        out = {}
+
+    return out
+
---
+++ worker/worker.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+import zmq
+import json
+
+from ssn_locations import ssn_to_state
+from dates import process_dates
+
+context = zmq.Context()
+receiver = context.socket(zmq.PULL)
+receiver.connect("tcp://localhost:5557")
+
+def process(deathfile_doc):
+    watermelon_doc = {
+        '_id': deathfile_doc['_id'],
+    }
+
+    for datetype in ['born', 'died']:
+        watermelon_doc.update(process_date(deathfile_doc[datetype], datetype))
+
+    watermelon_doc['state'] = ssn_to_state(deathfile_doc['ssn'])
+    watermelon_doc['middles.count'] = len(deathfile_doc['middles'])
+
+    return watermelon_doc
+
+while True:
+    # Get the document
+    _id = receiver.recv()
+    doc = db.deathfile.find_one({'_id': _id})
+
+    # Skip if we've already done this batch of processing.
+    watermelon = doc.get('watermelon', {})
+    if watermelon != {}:
+        continue # Already processed
+
+    # Otherwise, process it
+    command = {'$set': {'watermelon': process(doc)}}
+    db.deathfile.update({'ssn': doc['ssn']}, command)

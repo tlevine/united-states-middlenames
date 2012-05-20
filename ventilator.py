d377101e6bb584e0cd39db6e992e2f7afeddff56
Thomas Levine <thomas.levine@gmail.com>
2012-05-20 15:44:16 -0400

    watermelon extraction

---
+++ ventilator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+from pymongo import Connection
+import zmq
+import json
+
+connection = Connection()
+db = connection.middlenames
+
+context = zmq.Context()
+sender = context.socket(zmq.PUSH)
+sender.bind("tcp://*:5557")
+
+# Send out all of the document _ids
+for doc in db.deathfile.find():
+    sender.send(doc['_id'])

e608c78ca5ba2b5791d2d5f46c5bdb0d48a13289
Thomas Levine <thomas.levine@gmail.com>
2012-05-21 03:10:59 -0400

    parsed option

--- load/mongodb.py
+++ load/mongodb.py
@@ -9,4 +9,4 @@ db = connection.middlenames
 def store_in_db(rawline):
     'Save a particular line to the database.'
     ssn = rawline[1:10]
-    db.person_raw.save({'_id': ssn, 'rawline': rawline})
+    db.person_raw.save({'_id': ssn, 'rawline': rawline, 'parsed': False})

0b8b7fc9adccacde83702607a9c6e82c9e7361a8
Thomas Levine <thomas.levine@gmail.com>
2012-05-21 00:58:46 -0400

    enum types

--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
+CREATE TYPE weekday AS ENUM (
+  'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
+);
+CREATE TYPE month AS ENUM (
+  'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul',
+  'aug', 'sep', 'oct', 'nov', 'dec'
+);
+
 CREATE TABLE person_raw (
   ssn character(9) PRIMARY KEY,   -- Social security number
   rawline character(100) NOT NULL -- Raw line of the file
-)
+);
 
 CREATE TABLE person (
   -- Social security number
@@ -14,28 +22,24 @@ CREATE TABLE person (
   -- Date components
   born_year smallint,
   died_year smallint,
-  CHECK (died_year > born_year)
+  CHECK (died_year > born_year),
 
-  born_month smallint,
-  died_month smallint,
-  CHECK (12 >= born_month AND born_month >= 1)
-  CHECK (12 >= died_month AND died_month >= 1)
+  born_month month,
+  died_month month,
 
   born_day smallint,
   died_day smallint,
-  CHECK (31 >= born_day AND born_day >= 1)
-  CHECK (31 >= died_day AND died_day >= 1)
+  CHECK (31 >= born_day AND born_day >= 1),
+  CHECK (31 >= died_day AND died_day >= 1),
 
   -- The above date as a proper date
   born_date date,
   died_date date,
-  CHECK (died_date > born_date)
+  CHECK (died_date > born_date),
 
   -- Day of the week (0, 1, 2, 3, 4, 5 or 6)
-  born_dow smallint,
-  died_dow smallint,
-  CHECK (6 >= born_dow AND born_dow >= 0)
-  CHECK (6 >= died_dow AND died_dow >= 0)
+  born_dow weekday,
+  died_dow weekday,
 
   -- Date in the year 2000, in case the year is not known
   born_doy date,
@@ -43,4 +47,12 @@ CREATE TABLE person (
 
   state character(2) NOT NULL, -- Geographical state of registration
   middles_initials_count smallint NOT NULL -- How many middle initials
-)
+);
+
+CREATE INDEX surname ON person (surname)
+CREATE INDEX forename ON person (forename)
+
+CREATE INDEX born ON person (born_date);
+CREATE INDEX died ON person (died_date);
+
+CREATE INDEX state ON person (state);

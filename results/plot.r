1b18203c78ded0107e0794678ecdad82178ef945
Thomas Levine <occurrence@thomaslevine.com>
2012-05-27 01:53:38 -0400

    more plots

--- results/plot.r
+++ results/plot.r
@@ -2,4 +2,21 @@
 library(ggplot2)
 
 # read.csv('../queries/middlenames.csv')
-m <- read.csv('/tmp/middlenames.csv') # In RAM
+m.all <- read.csv('/tmp/middlenames.csv') # In RAM
+m.all <- na.omit(m.all)
+
+m.timeline<-function(m.df) {
+  ggplot(m.df) +
+    aes(x=year, y=(k/n), group=state, size=n, label=state) +
+#   scale_x_continuous(label='Birth year') +
+    geom_line()
+}
+
+# Each state separately
+timelines <- dlply(m.all, 'state', function(m.df) {
+  m.timeline(m.df) + 
+  opts(title=as.character(m.df[1, 'state']))
+})
+
+# All of the states
+timelines$all <- m.timeline(m.all)

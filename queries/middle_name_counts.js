d5580e5105e7da3afb6922639894f944431c3ba0
Thomas Levine <occurrence@thomaslevine.com>
2012-05-26 20:23:06 -0400

    map and reduce now return the same structure

--- queries/middle_name_counts.js
+++ queries/middle_name_counts.js
@@ -2,14 +2,17 @@ db.runCommand(
  { mapreduce : "person",
    map : function() {emit(
      [this.born_year, this.born_state],
-     this.middles.length > 0
+     {'k': this.middles.length > 0, 'n': 1}
    )},
    reduce : function(key, values) {
      // k = Number of people born with middle names
      // n = Number of people born, with or without middle names
-     var k = 0;
-     values.forEach(function(){k++});
-     return {"k": k, n: values.length};
+     var result={"k": 0, "n": 0};
+     values.forEach(function(value){
+       result.k+=value.k;
+       result.n+=value.n;
+     });
+     return result;
    },
    out: "middle_name_counts",
    jsMode : true,
